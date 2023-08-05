54cea83480679d097309c91ba47382478a422f8e
Jayamshriv <jayamshrivastav1@gmail.com>
2023-08-05 19:43:17 +0530

    password protection functionality added

--- app/src/main/java/com/example/notevault/ui/Adapter/NotesVaultHomeAdapter.kt
+++ app/src/main/java/com/example/notevault/ui/Adapter/NotesVaultHomeAdapter.kt
@@ -1,14 +1,20 @@
 package com.example.notevault.ui.Adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notevault.Models.NotesVault
 import com.example.notevault.R
 import com.example.notevault.databinding.ItemNotesBinding
 import com.example.notevault.ui.Activities.EditNote
+import kotlin.properties.Delegates
 
 class NotesVaultHomeAdapter(val context: Context?, var notes: List<NotesVault>?) :
     RecyclerView.Adapter<NotesVaultHomeAdapter.NotesVaultHomeHolder>() {
@@ -35,6 +41,7 @@ class NotesVaultHomeAdapter(val context: Context?, var notes: List<NotesVault>?)
         holder.binding.descTV.text = note.desc
         holder.binding.dateTV.text = note.date
 
+
         when (note.priority) {
             "1" -> {
                 holder.binding.priorityIV.setImageResource(R.drawable.low_priority_green)
@@ -47,20 +54,82 @@ class NotesVaultHomeAdapter(val context: Context?, var notes: List<NotesVault>?)
             }
         }
 
-        holder.binding.root.setOnClickListener {
-            val intent = Intent(holder.itemView.context, EditNote::class.java)
-            intent.putExtra("noteID", note.id)
-            intent.putExtra("noteTitle", note.title)
-            intent.putExtra("noteDesc", note.desc)
-            intent.putExtra("noteNote", note.note)
-            intent.putExtra("notePriority", note.priority)
-            holder.itemView.context.startActivity(intent)
-        }
+        holder.itemView.setOnClickListener {
 
+            if (note.password.isNotBlank()) {
+
+                Log.v("#####", "Passord in note")
+                passwordFromConfirmationDialog(holder.itemView.context, note) { ifPasswordCorrect ->
+                    Log.v("#####", "ifpasswordCorrect $ifPasswordCorrect")
+
+                    if (ifPasswordCorrect) {
+
+                        Log.v("#####", "ifpasswordCorrect  True 1 ${ifPasswordCorrect}")
+
+                        val intent = Intent(holder.itemView.context, EditNote::class.java)
+                        intent.putExtra("noteID", note.id)
+                        intent.putExtra("noteTitle", note.title)
+                        intent.putExtra("noteDesc", note.desc)
+                        intent.putExtra("noteNote", note.note)
+                        intent.putExtra("notePriority", note.priority)
+                        holder.itemView.context.startActivity(intent)
+
+                    } else {
+                        Log.v("#####", "ifpasswordCorrect  False  $ifPasswordCorrect")
+
+                        Toast.makeText(context, "Password galat hai bawa", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+            } else {
+
+                Log.v("#####", "No Password in note")
+
+                val intent = Intent(holder.itemView.context, EditNote::class.java)
+                intent.putExtra("noteID", note.id)
+                intent.putExtra("noteTitle", note.title)
+                intent.putExtra("noteDesc", note.desc)
+                intent.putExtra("noteNote", note.note)
+                intent.putExtra("notePriority", note.priority)
+                holder.itemView.context.startActivity(intent)
+            }
+        }
     }
+
 
     override fun getItemCount(): Int {
         return notes?.size ?: 0
     }
+
+    fun passwordFromConfirmationDialog(
+        context: Context,
+        noteWPassword: NotesVault,
+        onResult: (Boolean) -> Unit
+    ) {
+        val dialogView =
+            LayoutInflater.from(context).inflate(R.layout.password_confirmation_dialog, null)
+
+        val alertDialogBuilder = AlertDialog.Builder(context)
+            .setView(dialogView)
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+
+        dialogView.findViewById<TextView>(R.id.okPswdConfBtn).setOnClickListener {
+            val passwordET = dialogView.findViewById<EditText>(R.id.passwordFromDialog)
+            val passwordFromDialog = passwordET.text.toString().trim()
+
+            val isPasswordCorrect: Boolean = passwordFromDialog == noteWPassword.password
+            onResult(isPasswordCorrect)
+
+            alertDialog.dismiss()
+        }
+
+        dialogView.findViewById<TextView>(R.id.cancelPswdConfBtn).setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
+
 
 }
